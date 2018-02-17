cddee67e1712f5c323ea8bde2f9c317716779108
Raja Mummidi <rajamanohar.mummidi@gmail.com>
2018-02-16 23:07:43 -0500

    jquery files added

--- js/validation.js
+++ js/validation.js
@@ -1,24 +1,5 @@
 function login(){
 
-
-   /*
-
-   1. check email or passwd values should not be null  
-
-   2. check email id is proper format   @gmail.com 
-
-   3. Dabatabse calling --- check emailid/passwd combinations 
-
-
-null
-undefined
-NaN
-empty string ("")
-0
-false
-
-   */
-    // when calling, the values you pass are called arguments 
    	var emailId = document.getElementById('email').value
    	var passwd = document.getElementById('pass').value
 
@@ -33,37 +14,27 @@ false
    	 else 
    	 	alert("Hey! Your login is not successful")
 
-   	}
+}
 
-   	// function addition(x, y){  //x,y parameters 
 
-   	// 	return x + y 
-   	// }
-   	
+function checkEmailPasswdNullValues(email, passwd){
 
-    function checkEmailPasswdNullValues(email, passwd){
+  if( email.length >= 8 && passwd.length >= 6){
+		return true
+  }
+	else {
+		return false; 
+	}
+}
 
-        if( email.length >= 8 && passwd.length >= 6){
-    		return true
-        }
-    	else {
-    		return false; 
-    	}
-    }
+function checkEmailFormat(email){
 
-    function checkEmailFormat(email){
+var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+return re.test(String(email).toLowerCase());
 
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+}
 
-    }
-
-    function checkEmailPasswdInDatabase(emailId, passwd)
-    {
-
-    	/*
-            database calling from javascript 
-    	*/
-
-    	return false;
-    }
+function checkEmailPasswdInDatabase(emailId, passwd)
+{
+	return false;
+}
